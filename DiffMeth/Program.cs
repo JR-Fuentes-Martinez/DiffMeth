bada758558fa6bef112eaf3aba885f439f39e9d7
JR_Fuentes_Martinez <jrfm.verboran@outlook.es>
2023-03-03 22:12:02 +0100

    Tri-axis Sin function

--- DiffMeth/Program.cs
+++ DiffMeth/Program.cs
@@ -1,23 +1,25 @@
 ﻿using LibDiffMeth;
 using Plotly.NET;
 
+const int Iteraciones = 500;
+
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-double[] ys = new double[100];
-for (int i = 0; i < 100; i++)
+double[] ys = new double[Iteraciones];
+for (int i = 0; i < Iteraciones; i++)
 {
-    ys[i] = Math.Pow(i, 2);
+    ys[i] = Math.Sin(i * (Math.PI /180));
 }
-
 Diferentiator Diff = new Diferentiator(){
     Multiplo = 1,
     ys = ys,
     Penalizada = false,
-    FactorPenalty = +3.0
+    FactorPenalty = +3.0,
+    TipoExtremos = "p"  //"n" => natural    "p" => parabólicos (por defecto)
 };
 var res = Diff.MakeSplineDiff();
 if (res != null) {
     var graf = Diff.Make_Graf(res);
-    graf.Show();
+    graf.SaveHtml("graf.html", true);
 }
--- LibDiffMeth/ClsMath.cs
+++ LibDiffMeth/ClsMath.cs
@@ -7,23 +7,27 @@ using Microsoft.FSharp.Collections;
 namespace LibDiffMeth;
 
 public class Diferentiator
-{    
+{
     public double[]? ys {get; set;}
     public int Multiplo {get; set;} = 5;
     public bool Penalizada {get; set;} = false;
     public double FactorPenalty {get; set;} = 0.0;
     public double FactorEscala {get; protected set;} = 0.0;
+    public string TipoExtremos {get; set;} = "p";
 
     public List<double[]>? MakeSplineDiff()
     {
-        List<double[]> Retorna = new List<double[]>(3);
+        List<double[]> Retorna = new List<double[]>(4);
 
         if (ys == null || (Penalizada && (FactorPenalty < -15.0 || FactorPenalty > 15.0))) {
             return null;
         }
         try {
+            int BoundT = 0;
             double[] xs = new double[ys.Length];
             double[] xs2 = new double[ys.Length * Multiplo];
+
+            if (TipoExtremos.ToLower() == "n") BoundT = 1;
 
             for (int i = 0; i < ys.Length; i++)
             {
@@ -38,8 +42,7 @@ public class Diferentiator
             alglib.spline1dinterpolant inter;
             alglib.spline1dfitreport repo;
             int info;
-
-            alglib.spline1dconvdiffcubic(xs, ys, xs.Length, 0, 0.0, 0, 0.0, xs2, xs2.Length, out f1, out d1);
+            alglib.spline1dconvdiffcubic(xs, ys, xs.Length, BoundT, 0.0, BoundT, 0.0, xs2, xs2.Length, out f1, out d1);
 
             double[] d1pen = new double[xs2.Length];
 
@@ -54,8 +57,8 @@ public class Diferentiator
             else {
                 d1pen = d1;
             }
+            double[] f1Dev = new double[xs2.Length];      
 
-            double[] f1Dev = new double[xs2.Length];           
             for (int i = 0; i < xs2.Length; i++)
             {
                 double suma = 0.0;
@@ -67,6 +70,7 @@ public class Diferentiator
             }
             var Primerof1 = f1[0];
             var Primerof1Dev = f1Dev[0];
+
             for (int i = 0; i < xs2.Length; i++)
             {
                 f1Dev[i] -= Primerof1Dev;
@@ -75,6 +79,7 @@ public class Diferentiator
             Retorna.Add(xs2);
             Retorna.Add(f1);
             Retorna.Add(f1Dev);
+            Retorna.Add(d1pen);
 
             return Retorna;
         }
@@ -84,31 +89,36 @@ public class Diferentiator
     }
     public GenericChart.GenericChart Make_Graf(List<double[]> Lista) {
 
-        string Xunit, Yunit, Y2unit, Titulo;
+        string Xunit, Yunit, Y2unit, Y3unit, Titulo;
 
         Yunit = "u(y)";
-        Y2unit = "Acum [u(y) : u(x)]";
-        Titulo = "Funcion y acumulacion en base a derivada";        
+        Y3unit = "u(y) : u(x)";
+        Y2unit = "Acum [ u(y) : u(x) ]";
+        Titulo = "Función y acumulación en base a derivada";        
         Xunit = "u(x)";
 
         string ColorF = "blue";
         string ColorD = "maroon";
+        string ColorFD = "green";
         
         LinearAxis xAxis = new();
-        xAxis.SetValue("title", $"{Xunit}");
+        xAxis.SetValue("title", Xunit);
         xAxis.SetValue("showgrid", false);
         xAxis.SetValue("showline", true);
+        xAxis.SetValue("domain", new double[]{0.25, 0.75});
 
         Font Fuente1 = new();
         Fuente1.SetValue("color",ColorF);
         Font Fuente2 = new();
         Fuente2.SetValue("color", ColorD);
+        Font Fuente3 = new();
+        Fuente3.SetValue("color", ColorFD);
         Font FTitulo = new();
-        FTitulo.SetValue("size", 14);
+        FTitulo.SetValue("size", 18);
 
         LinearAxis yAxis = new();
-        yAxis.SetValue("title", $"{Yunit}");
-        yAxis.SetValue("showgrid", false);
+        yAxis.SetValue("title", Yunit);
+        yAxis.SetValue("showgrid", true);
         yAxis.SetValue("showline", true);
         yAxis.SetValue("linecolor", ColorF);
         yAxis.SetValue("titlefont", Fuente1);
@@ -116,8 +126,8 @@ public class Diferentiator
         yAxis.SetValue("side", "left");
 
         LinearAxis yAxis2 = new();
-        yAxis2.SetValue("title", $"{Y2unit}");
-        yAxis2.SetValue("showgrid", false);
+        yAxis2.SetValue("title", Y2unit);
+        yAxis2.SetValue("showgrid", true);
         yAxis2.SetValue("showline", true);
         yAxis2.SetValue("linecolor", ColorD);
         yAxis2.SetValue("titlefont", Fuente2);
@@ -125,6 +135,18 @@ public class Diferentiator
         yAxis2.SetValue("overlaying", "y");
         yAxis2.SetValue("side", "right");
 
+        LinearAxis yAxis3 = new();
+        yAxis3.SetValue("title", Y3unit);
+        yAxis3.SetValue("showgrid", false);
+        yAxis3.SetValue("showline", true);
+        yAxis3.SetValue("linecolor", ColorFD);
+        yAxis3.SetValue("titlefont", Fuente3);
+        yAxis3.SetValue("tickfont", Fuente3);
+        yAxis3.SetValue("overlaying", "y");
+        yAxis3.SetValue("side", "right");
+        yAxis3.SetValue("anchor", "free");
+        yAxis3.SetValue("position", 0.8);
+       
         Legend Leyenda = new();
         Leyenda.SetValue("orientation", "h");
 
@@ -132,7 +154,7 @@ public class Diferentiator
         Titul.SetValue("xanchor", "center");
         Titul.SetValue("yanchor", "top");
         Titul.SetValue("x", 0.5);
-        Titul.SetValue("y", 0.9);
+        Titul.SetValue("y", 0.95);
         Titul.SetValue("font", FTitulo);
 
         Layout layout = new Layout();  
@@ -140,13 +162,17 @@ public class Diferentiator
         layout.SetValue("xaxis", xAxis);
         layout.SetValue("yaxis", yAxis);
         layout.SetValue("yaxis2", yAxis2);
+        layout.SetValue("yaxis3", yAxis3);
         layout.SetValue("legend", Leyenda);    
         layout.SetValue("showlegend", true);
+        layout.SetValue("autosize", true);
 
         Line T1 = new();
         T1.SetValue("color", ColorF);
         Line T2 = new();
         T2.SetValue("color", ColorD);
+        Line T3 = new();
+        T3.SetValue("color", ColorFD);
 
         Trace trace1 = new Trace("scatter");
         trace1.SetValue("x", Lista[0]);
@@ -159,19 +185,28 @@ public class Diferentiator
         trace2.SetValue("x", Lista[0]);
         trace2.SetValue("y", Lista[2]);
         trace2.SetValue("mode", "line");
-        trace2.SetValue("name", "Derivada");
+        trace2.SetValue("name", "Acumulacion derivada");
         trace2.SetValue("yaxis", "y2");
         trace2.SetValue("line", T2);
 
-        FSharpList<Trace> niceSharpList = ListModule.OfSeq(new List<Trace>(){trace1, trace2});
-        var Configura = Config.init(ToImageButtonOptions: ToImageButtonOptions.init(Format: StyleParam.ImageFormat.SVG));
+        Trace trace3 = new Trace("scatter");
+        trace3.SetValue("x", Lista[0]);
+        trace3.SetValue("y", Lista[3]);
+        trace3.SetValue("mode", "line");
+        trace3.SetValue("name", "Derivada");
+        trace3.SetValue("yaxis", "y3");
+        trace3.SetValue("line", T3);
 
-        var retorna =  GenericChart
-            .ofTraceObjects(true, niceSharpList)
+        FSharpList<Trace> niceSharpList = ListModule.OfSeq(new List<Trace>(){trace1, trace2, trace3});
+        var Configura = Config.init(ToImageButtonOptions: ToImageButtonOptions.init(Format: StyleParam.ImageFormat.SVG),
+            Responsive: true, FillFrame: true, Autosizable: true);
+
+        var retorna =  GenericChart            
+            .ofTraceObjects(false, niceSharpList)
             .WithLayout(layout)
             .WithTitle(Titulo)
             .WithConfig(Configura);
-        
+
         return retorna;
     }
 }
